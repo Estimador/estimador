784784a50b128a2dfba4b0e5522f4648c8015ff6
Estimador <estimador.transferencia@gmail.com>
2024-07-09 17:30:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ app.get('/consulta', async (req, res) => {
   }
 
   try {
-const browser = await puppeteer.launch({
-  executablePath: '/usr/bin/google-chrome-stable',
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
-});
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
